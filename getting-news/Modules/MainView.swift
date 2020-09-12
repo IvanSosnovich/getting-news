227aaa731e53428a04d93ed7970feaca7b533739
IbanSosnovich <ivan.sosnovich@inbox.ru>
2020-09-13 00:16:13 +0300

    created a project as well as a network layer

---
+++ getting-news/Modules/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  getting-news
+//
+//  Created by MIkkyMouse on 12.09.2020.
+//  Copyright © 2020 Ivan Sosnovich. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+    
+    
+    
+}
